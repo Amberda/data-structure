02056d07c542ee2fec6a16a5f804d0e2b27f90c5
Amberda <18729219705@163.com>
2018-04-06 20:21:42 +0800

    RBT

---
+++ rbtest.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "RBTree.h"
+
+void Test()
+{
+	int a[9] = { 10, 7, 8, 15, 5, 6, 11, 13, 12 };
+	RBTree<int, int> brt;
+
+	for (int i = 0; i < 9; i++)
+	{
+		brt.Insret(a[i], i);
+	}
+
+	brt.InOrder();
+	cout << "IsRBTree?" << brt.IsRBTree() << endl;
+}
+
+int main()
+{
+	Test();
+	system("pause");
+	return 0;
+}
